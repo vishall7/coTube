aad42ce8697ca284b3fc734cdc43658f84e3af83
vishall7 <vishalbarve2305@gmail.com>
2024-04-01 19:39:23 +0530

    created room schema

---
+++ src/controllers/room.controller.js
@@ -0,0 +1,5 @@
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiResponse";
+import { Room } from "../models/room.model";
+
---
+++ src/models/room.model.js
@@ -0,0 +1,39 @@
+import mongoose, {Schema}  from "mongoose";
+import jwt from "jsonwebtoken";
+
+
+const roomSchema = new Schema(
+    {
+        roomName: {
+            type: String,
+            lowercase: true,
+            trim: true,
+            required: true,
+        },
+        createdBy: {
+            type: mongoose.Types.ObjectId,
+            ref: "User",            
+            required: true
+        },
+        timeLimit: {
+            type: Number,
+            default: 30
+        }
+    }
+);
+
+roomSchema.methods.generateToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            roomName: this.roomName,
+            createdBy: this.createdBy,            
+        },
+        process.env.ROOM_TOKEN_SECRET,
+        {
+            expiresIn: this.timeLimit + "m"
+        }
+    )
+}
+
+export const Room = mongoose.model("Room",roomSchema);
